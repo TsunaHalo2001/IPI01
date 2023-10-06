b3f65f77573ba6522d901c8652ab975c30383b3a
TsunaHalo2001 <145141553+TsunaHalo2001@users.noreply.github.com>
2023-10-06 09:50:20 -0500

    addesd ejercicio moneda

---
+++ EjercicioConvertirMoneda.py
@@ -0,0 +1,40 @@
+_ARG = 0.0029
+_COP = 0.00023
+_MXN = 0.055 
+
+def convert(conversion):
+    value = float(input("Enter the quantity "))
+    USD = value*conversion
+    return USD
+
+def main():
+    print("Welcome to the money converter")
+    
+    while (True):
+        print("Select your currency")
+        print("1. ARG")
+        print("2. COP")
+        print("3. MXN")
+        print("4. Close")
+        
+
+        option = int(input())
+
+        if(option == 1):
+            USD = convert(_ARG)
+        elif(option == 2):
+            USD = convert(_COP)
+        elif(option == 3):
+            USD = convert(_MXN)
+        elif(option == 4):
+            return
+        else:
+            print("Invalid option")
+
+        if(option >= 1 and option <=3):
+            print("The converted value is ",USD," USD")
+        
+
+
+if __name__ == "__main__":
+    main()
